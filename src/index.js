8582a29de48bc68d90d20c5df4ce8cd1aaaa8393
Abhinay S. Bhadauria <knowledgepoint887@gmail.com>
2024-02-15 14:58:44 +0530

    Setup optimisation SMTP using nodemailer

--- src/index.js
+++ src/index.js
@@ -3,15 +3,25 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
+const { sendBasicEmail } = require('./services/email-service');
+
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
     app.listen(PORT, () => {
-        console.log(`Server started on PORT: ${PORT}`);
+        console.log(`Server started at port ${PORT}`);
+
+        sendBasicEmail(
+            'mroverledge@gmail.com',
+            'abhinaybhadauria99@gmail.com',
+            'This is a testing email',
+            'Hey, how are you, I hope you like the support'
+        );
     });
 }
 
-
 setupAndStartServer();
+
+
